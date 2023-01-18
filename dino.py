df465a75c288a4a0048b9aab807fd889d18c845c
polterg3ist <selimovsultan@gmail.com>
2023-01-18 22:40:48 +0300

    randomized dino flip directions

--- dino.py
+++ dino.py
@@ -1,4 +1,5 @@
 import pygame
+from random import choice
 
 
 class Dino:
@@ -12,19 +13,24 @@ class Dino:
         self.jumps = 0
         self.is_jump = False
         self.is_falling = False
-        self.angle = 1
+        self.angle = 0
+        self.flips = ["back-flip", "forward-flip"]
+        self.flip = choice(self.flips)
 
     def output(self):
         if self.jumps == 2:
-            self.blit_rotate_dino(self.angle)
-            self.angle += 10
+            self.blit_rotate_dino()
         else:
             self.screen.blit(self.image, self.rect)
 
-    def blit_rotate_dino(self, angle):
-        rotated_image = pygame.transform.rotate(self.image, angle)
+    def blit_rotate_dino(self):
+        rotated_image = pygame.transform.rotate(self.image, self.angle)
         new_rect = rotated_image.get_rect(center=self.image.get_rect(topleft=self.rect.topleft).center)
         self.screen.blit(rotated_image, new_rect.topleft)
+        if self.flip == "back-flip":
+            self.angle += 10
+        elif self.flip == "forward-flip":
+            self.angle -= 10
 
     def update(self):
         if self.is_jump:
@@ -44,4 +50,5 @@ class Dino:
                 self.speed_y = 15
                 self.rect.bottom = self.screen_rect.bottom
                 self.jumps = 0
-                self.angle = 1
+                self.angle = 0
+                self.flip = choice(self.flips)
